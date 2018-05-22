7c3117144201bb4a446591903f4577f25189699a
dvillaseno10 <38872725+dvillaseno10@users.noreply.github.com>
2018-05-22 15:39:05 -0700

    Update CarWash.cpp

--- CarWash.cpp
+++ CarWash.cpp
@@ -7,8 +7,8 @@
 using namespace std;
 
 //global declaration statements
-void totalPrice(double &total);
-double add(int);
+double * add(int);
+void welcomePrompt(int*, double*);
 //array which stores prices for services
 double prices[2][4]= {{8.99, 14.99, 18.99, 22.99 }, { 1.50, 5.00, 10.00, 30.00 }};
 double choices;
@@ -68,19 +68,21 @@ int linear_search(int arr[], int n, int srch)
   }
   return -1;
 }
+
 int main()
 {
-  srand(time(0));
-  // main declaration statements
-
+//declaration statements
 	int choice = 0;
 	double total = 0;
 	double washes;
 	int tempID = 0;
   int LogReg = 0;
+
 	getinfo();
-	
+
   selection_sort(UsrName,idNum,size);
+
+  srand(time(0));
 
   ofstream out;
 	out.open("receipt.txt");
@@ -118,6 +120,87 @@ int main()
     exit(0);
   }
 
+  welcomePrompt (&choice, &washes);
+  out << washes << "x";
+
+	//Switch statement uses the input to then use other functions to sum the total
+	switch (choice)
+	{
+	case 1:
+		cout << "\nBronze selected\nThis package includes:\n*Deluxe Wash\n*Tire Clean\n" << endl;
+    out << "Bronze  \t@$" << prices[0][0] << "\t\t$" << prices[0][0] * washes << endl;
+		total += prices[0][0] * washes;
+		total += *add(choices);
+		cout << "The total price comes out too : $" << total << endl;
+		break;
+	case 2:
+		cout << "\nSilver selected\nThis package includes:\n*Deluxe Wash\n*Tire Clean\n*Interior Vacumm\n" << endl;
+    out << "Silver  \t@$" << prices[0][1] << "\t\t$" << prices[0][1] * washes << endl;
+		total += prices[0][1] * washes;
+		total += *add(choices);
+		cout << "The total price comes out to : $" << total << endl;
+		break;
+	case 3:
+		cout << "\nGold selected\nThis package includes:\n*Deluxe Wash\n*Tire Clean\n*Interior Vacumm\n*Coat Wax\n" << endl;
+    out << "Gold    \t@$" << prices[0][2] << "\t\t$" << prices[0][2] * washes << endl;
+		total += prices[0][2] * washes;
+		total += *add(choices);
+		cout << "The total price comes out to : $" << total << endl;
+		break;
+	case 4:
+		cout << "\nPlatinum selected\nThis package includes:\n*Deluxe Wash\n*Interior Vacumm\n*Coat Wax\n*Tire Shine\n*Complete Surface Protectant\n" << endl;
+    out << "Platinum\t@$" << prices[0][3] << "\t\t$" << prices[0][3] * washes << endl;
+		total += prices[0][3] * washes;
+		total += *add(choices);
+		cout << "The total price comes out too : $" << total << endl;
+		break;
+	default:
+		cout << "Invalid input select options 1-4" << endl;
+		}
+	//output total to receipt.txt file 
+  out << "additional services   $" << fixed << setprecision(2) << addSer << endl;
+	out << "\ntotal: \t\t\t\t\t\t\t\t$" << total;
+	out.close();
+
+  ofstream put;
+  put.open("data.txt");
+    
+  if (LogReg == 1)
+  {
+  for(int i = 0; i < size + 1; i++)
+    {
+      put << UsrName[i] << endl;
+      put << idNum[i]<< endl;
+    }
+  put.close();
+  }
+  else if (LogReg == 2)
+  {
+    for(int i = 0; i < size; i++)
+    {
+      put << UsrName[i] << endl;
+      put << idNum[i]<< endl;
+    }
+  put.close();
+  }
+}
+
+int showPrompt()
+{
+  int choice = 0;
+  cout << "Would you like any additional services?\n";
+  cout << "---------------------------------------\n";
+  cout << "1. Air Freshener              $1.50\n";
+  cout << "2. Tire Pump                  $5.00\n";
+  cout << "3. Rotation of Tires          $10.00\n";
+  cout << "4. Oil Change plus Filter     $30.00\n";
+  cout << "5. No additional services\n";
+  cin >> choice;
+  return choice;
+}
+
+void welcomePrompt(int *choice, double *washes)
+{
   cout << "\nWelcome " << UsrName[y] << ",";
 	cout << "\nOur custom washes are listed below\n";
 	cout << "------------------------------------\n";
@@ -126,125 +209,49 @@ int main()
 	cout << "2.Sliver\t\t$" << prices[0][1] << endl;
 	cout << "3.Gold  \t\t$" << prices[0][2] << endl;
 	cout << "4.Platinum\t\t$" << prices[0][3] << endl << endl;
-		cout << "First off, How many cars will we be servicing today? ";
-		cin >> washes;
-    out << washes << "x";
-		cout << "Great! What will your choice of wash be today?\n (Select options 1 - 4): ";
-		cin >> choice;
-		//Switch statement uses the input to then use other functions to sum the total
-		switch (choice)
-		{
-		case 1:
-			cout << "\nBronze selected\nThis package includes:\n*Deluxe Wash\n*Tire Clean\n" << endl;
-      out << "Bronze  \t@$" << prices[0][0] << "\t\t$" << prices[0][0] * washes << endl;
-			total += prices[0][0] * washes;
-			total += add(choices);
-			cout << "The total price comes out too : $" << total << endl;
-			break;
-		case 2:
-			cout << "\nSilver selected\nThis package includes:\n*Deluxe Wash\n*Tire Clean\n*Interior Vacumm\n" << endl;
-      out << "Silver  \t@$" << prices[0][1] << "\t\t$" << prices[0][1] * washes << endl;
-			total += prices[0][1] * washes;
-			total += add(choices);
-			cout << "The total price comes out to : $" << total << endl;
-			break;
-		case 3:
-			cout << "\nGold selected\nThis package includes:\n*Deluxe Wash\n*Tire Clean\n*Interior Vacumm\n*Coat Wax\n" << endl;
-      out << "Gold    \t@$" << prices[0][2] << "\t\t$" << prices[0][2] * washes << endl;
-			total += prices[0][2] * washes;
-			total += add(choices);
-			cout << "The total price comes out to : $" << total << endl;
-			break;
-		case 4:
-			cout << "\nPlatinum selected\nThis package includes:\n*Deluxe Wash\n*Interior Vacumm\n*Coat Wax\n*Tire Shine\n*Complete Surface Protectant\n" << endl;
-      out << "Platinum\t@$" << prices[0][3] << "\t\t$" << prices[0][3] * washes << endl;
-			total += prices[0][3] * washes;
-			total += add(choices);
-			cout << "The total price comes out too : $" << total << endl;
-			break;
-		default:
-			cout << "Invalid input select options 1-4" << endl;
-		}
-		//output total to receipt.txt file 
-    out << "additional services   $" <<fixed << setprecision(2) << addSer << endl;
-		out << "\ntotal: \t\t\t\t\t\t\t\t$" << total;
-		out.close();
-    ofstream put;
-    put.open("data.txt");
-    if (LogReg == 1)
-    {
-    for(int i = 0; i < size + 1; i++)
-      {
-        put << UsrName[i] << endl;
-        put << idNum[i]<< endl;
-      }
-    put.close();
-    }
-    else if (LogReg == 2)
-    {
-      for(int i = 0; i < size; i++)
-      {
-        put << UsrName[i] << endl;
-        put << idNum[i]<< endl;
-      }
-    put.close();
-    }
+	cout << "First off, How many cars will we be servicing today? ";
+	cin >> *washes;
+	cout << "Great! What will your choice of wash be today?\n (Select options 1 - 4): ";
+	cin >> *choice;
 }
 
 // This function adds choices for additional prices to total
-double add(int choices)
+double * add(int choices)
 {
-    double total = 0;
+  double *total;
 		while(true)
 		{
-		  // Display texts and prices for additional features user to select 1-5
-		  cout << "Would you like any additional services?\n";
-  		cout << "---------------------------------------\n";
-  		cout << "1. Air Freshener              $1.50\n";
-  		cout << "2. Tire Pump                  $5.00\n";
-  		cout << "3. Rotation of Tires          $10.00\n";
-  		cout << "4. Oil Change plus Filter     $30.00\n";
-  		cout << "5. No additional services\n";
-  		cin >> choices;
-  		//if else loop which runs when user inputs numbers 1-4 and ends with 5 when user no longer wants to add anymore features
-		  if(choices == 1)
-		  {
-		    cout << "'Air Freshner added'" << endl;
-  			cout << "An additional $1.50 will be charged." << endl;
-  			total += prices[1][0];
-        addSer += prices[1][0];
-		  }
-		  else if (choices == 2)
-		  {
-		    cout << "'Tire Pump added'" << endl;
-  			cout << "An additional $5.00  will be charged\n" << endl;
-  			total += prices[1][1];
-        addSer += prices[1][1];
-		  }
-		  else if (choices == 3)
-		  {
-		    cout << "'Rotation of Tires added'" << endl;
-  			cout << "An additional $10.00 will be charged\n" << endl;
-  			total += prices[1][2];
-        addSer += prices[1][2];
-		  }
-		  else if (choices == 4)
-		  {
-		    cout << "'Oil Change added'" << endl;
-  			cout << "An additional $30.00 will be charged\n" << endl;
-  			total += prices[1][3];
-        addSer += prices[1][3];
-		  }
-		  else if (choices == 5)
-		  {
-		    break;
-		  }
-		  else
-		  {
-		    cout << "Please enter a number 1 - 5\n";
-		  }
-
-		}
-		return total;
-    return addSer;
+      switch(choices = showPrompt())
+      {
+        case 1: 
+          cout << "'Air Freshner added'" << endl;
+          cout << "An additional $1.50 will be charged." << endl;
+          *total += prices[1][0];
+          addSer += prices[1][0];
+          continue;
+        case 2:
+          cout << "'Tire Pump added'" << endl;
+          cout << "An additional $5.00  will be charged\n" << endl;
+          *total += prices[1][1];
+          addSer += prices[1][1];
+          continue;
+        case 3:
+          cout << "'Rotation of Tires added'" << endl;
+          cout << "An additional $10.00 will be charged\n" << endl;
+          *total += prices[1][2];
+          addSer += prices[1][2];
+          continue;
+        case 4:
+          cout << "'Oil Change added'" << endl;
+          cout << "An additional $30.00 will be charged\n" << endl;
+          *total += prices[1][3];
+          addSer += prices[1][3];
+          continue;
+        case 5:
+          	return total;
+        default :
+          cout << "Please enter a number 1 - 5\n";
+      }
+    }
+		return nullptr;
 }
